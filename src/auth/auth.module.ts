ac038606a3574089f54065fe396cbcfe3e787f13
Bernard Teye <bernard.teye@amalitech.com>
2024-07-01 16:17:51 +0000

    import mailer.module.ts into auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MailerModule } from 'src/mailer/mailer.module';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [
+    MailerModule
+  ]
 })
 export class AuthModule {}
