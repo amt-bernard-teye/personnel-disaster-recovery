aa7e5ffed69341272e14bfe1d6761d3e20ccd42b
Bernard Teye <bernard.teye@amalitech.com>
2024-07-01 13:16:11 +0000

    add seeders module and import into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { SeedersModule } from './seeders/seeders.module';
 
 @Module({ 
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
     JwtModule.register({global: true}),
     DatabaseModule,
-    AuthModule
+    AuthModule,
+    SeedersModule
   ]
 })
 export class AppModule {}
