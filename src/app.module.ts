9512db6d46b742ad0e6968634f265ed155cb172e
Bernard Teye <bernard.teye@amalitech.com>
2024-07-01 11:53:32 +0000

    add auth.module.ts and import into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({ 
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
     JwtModule.register({global: true}),
-    DatabaseModule
+    DatabaseModule,
+    AuthModule
   ]
 })
 export class AppModule {}
