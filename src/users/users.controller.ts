46e2a633897062d947e20a50054dacd6ce8174b2
Bernard Teye <bernard.teye@amalitech.com>
2024-07-03 11:01:56 +0000

    add checkEmail route handler to UsersController.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 
@@ -6,9 +6,21 @@ import { AuthGuard } from 'src/shared/auth.guard';
 import { DataOnlyInterceptor } from 'src/shared/interceptors/data-only.interceptor';
 import { swaggerInternalError } from 'src/shared/swagger/internal-error.swagger';
 import { swaggerActiveSuccess } from './swagger/active.swagger';
+import { ChangePersonalInfoDto } from './dto/change-personal-info.dto';
+import { UsersService } from './users.service';
+import { User } from 'src/shared/interface/user.interface';
+import { ResponseMessage } from 'src/shared/decorators/response-message.decorator';
+import { DataMessageInterceptor } from 'src/shared/interceptors/data-message.interceptor';
+import { ChangeEmailDto } from './dto/change-email.dto';
+import { MessageOnlyInterceptor } from 'src/shared/interceptors/message-only.interceptor';
+import { swaggerCheckEmailSuccess, swaggerCheckEmailValidationError } from './swagger/check-email.swagger';
 
 @Controller('users')
 export class UsersController {
+    constructor(
+        private usersService: UsersService
+    ) { }
+
     @Get("logged-in")
     @UseGuards(AuthGuard)
     @UseInterceptors(DataOnlyInterceptor)
@@ -19,5 +31,30 @@ export class UsersController {
     loggedInUser(@Req() request: Request) {
         const {password, ...user} = request["user"];
         return user;
+    }
+
+    @Post("personal")
+    @UseGuards(AuthGuard)
+    @ResponseMessage("Successfully updated personal info")
+    @UseInterceptors(DataMessageInterceptor)
+    @ApiTags("Users")
+    @ApiBearerAuth()
+    @ApiResponse(swaggerInternalError)
+    changePersonalInfo(@Req() request: Request, @Body() body: ChangePersonalInfoDto) {
+        const user = <User>request["user"];
+        return this.usersService.changePersonalInfo(body.name, body.email, user);
+    }
+
+    @Post("check-email")
+    @UseGuards(AuthGuard)
+    @UseInterceptors(MessageOnlyInterceptor)
+    @ApiTags("Users")
+    @ApiBearerAuth()
+    @ApiResponse(swaggerInternalError)
+    @ApiResponse(swaggerCheckEmailSuccess)
+    @ApiResponse(swaggerCheckEmailValidationError)
+    checkEmail(@Req() request: Request, @Body() body: ChangeEmailDto) {
+        const user = <User>request["user"];
+        return this.usersService.checkEmail(body.email, user);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { UserRepository } from 'src/database/repository/user.repository';
+import { User } from 'src/shared/interface/user.interface';
 
 @Injectable()
 export class UsersService {
 
     constructor(
-        private configService: ConfigService,
+        private userRepo: UserRepository
     ) { }
 
-    
+    async changePersonalInfo(name: string, email: string, existingUser: User) {
+        try {
+            const fetchedUser = await this.userRepo.find(email);
+
+            if (fetchedUser && fetchedUser.email !== existingUser.email) {
+                throw new BadRequestException("Email already exist");
+            }
+
+            existingUser.name = name;
+            existingUser.email = email;
+
+            const updatedUser = await this.userRepo.update(existingUser);
+            const {password, ...user} = updatedUser;
+
+            return user;
+        }
+        catch(error) {
+            if (error instanceof BadRequestException) {
+                throw new BadRequestException(error.message);
+            }
+            throw new InternalServerErrorException("Something went wrong");
+        }
+    }
+
+    async checkEmail(email: string, user: User) {
+        try {
+            const fetchedUser = await this.userRepo.find(email);
+
+            if (fetchedUser && fetchedUser.email !== user.email) {
+                throw new BadRequestException("Email already exist, try a different one");
+            }
+
+            return "Email doesn't exist, you are free to use it";
+        }
+        catch(error) {
+            if (error instanceof BadRequestException) {
+                throw new BadRequestException(error.message);
+            }
+            throw new InternalServerErrorException("Something went wrong");
+        }
+    }
 }
