e4a1b1cc8543e58e2298895ab78ec7c022bd2769
Bernard Teye <bernard.teye@amalitech.com>
2024-07-08 07:42:05 +0000

    add personnel service and import into personnel module

--- src/personnel/personnel.module.ts
+++ src/personnel/personnel.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PersonnelController } from './personnel.controller';
+import { PersonnelService } from './personnel.service';
 
 @Module({
-  controllers: [PersonnelController]
+  controllers: [PersonnelController],
+  providers: [PersonnelService]
 })
 export class PersonnelModule {}
