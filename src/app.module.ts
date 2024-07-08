b5e737fcdae355d4b385031dfdf0b213af3a32f9
Bernard Teye <bernard.teye@amalitech.com>
2024-07-08 07:38:45 +0000

    add personnel module and import into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { SeedersModule } from './seeders/seeders.module';
 import { SharedModule } from './shared/shared.module';
 import { MailerModule } from './mailer/mailer.module';
 import { UsersModule } from './users/users.module';
+import { PersonnelModule } from './personnel/personnel.module';
 
 @Module({ 
   imports: [
@@ -19,6 +20,7 @@ import { UsersModule } from './users/users.module';
     SharedModule,
     MailerModule,
     UsersModule,
+    PersonnelModule,
   ]
 })
 export class AppModule {}
