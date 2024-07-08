36bde17223eec40503b3f93995bb4005ce2a976e
Bernard Teye <bernard.teye@amalitech.com>
2024-07-08 11:08:40 +0000

    import EmergencyTypeRepository into database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,20 @@
 import { Global, Module } from '@nestjs/common';
+
 import { UserRepository } from './repository/user.repository';
+import { PersonnelRepository } from './repository/personnel.repository';
+import { EmergencyTypeRepository } from './repository/emergency-type.repository';
 
 @Global()
 @Module({
     providers: [
-        UserRepository
+        UserRepository,
+        PersonnelRepository,
+        EmergencyTypeRepository
     ],
     exports: [
-        UserRepository
+        UserRepository,
+        PersonnelRepository,
+        EmergencyTypeRepository
     ]
 })
 export class DatabaseModule {}
