067596d5fde6fea3541cd451e72eece81afd8272
Bernard Teye <bernard.teye@amalitech.com>
2024-07-08 08:20:56 +0000

    add personnel.interface.ts containing 2 types namely: Personnel and PersonnelProp

---
+++ src/shared/interface/personnel.interface.ts
@@ -0,0 +1,21 @@
+export type Personnel = {
+    id?: number;
+    phoneNumber: string;
+    houseNumber: string;
+    gender: string;
+    dob: string;
+    town: string;
+    digitalAddress: string;
+    userId: string;
+}
+
+export type PersonnelProp = {
+    id: boolean;
+    phoneNumber: boolean;
+    houseNumber: boolean;
+    gender: boolean;
+    dob: boolean;
+    town: boolean;
+    digitalAddress: boolean;
+    userId: boolean;
+}
