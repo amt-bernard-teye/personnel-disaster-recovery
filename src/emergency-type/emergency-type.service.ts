ea55696ec1b49edaa4f50243b6d2c2071d729bc4
Bernard Teye <bernard.teye@amalitech.com>
2024-07-08 11:59:22 +0000

    refactor create() in emergency-type.service.ts

--- src/emergency-type/emergency-type.service.ts
+++ src/emergency-type/emergency-type.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { EmergencyTypeRepository } from 'src/database/repository/emergency-type.repository';
 
 @Injectable()
@@ -7,11 +7,21 @@ export class EmergencyTypeService {
 
     async create(name: string) {
         try {
+            const existingType = await this.emergencyTypeRepo.find(name);
+
+            if (existingType) {
+                throw new BadRequestException("Emergency type already exist");
+            }
+
             return await this.emergencyTypeRepo.add({
                 name: name
             });
         }
         catch(error) {
+            if (error instanceof BadRequestException) {
+                throw new BadRequestException(error.message);
+            }
+
             throw new InternalServerErrorException("Something went wrong");
         }
     }
