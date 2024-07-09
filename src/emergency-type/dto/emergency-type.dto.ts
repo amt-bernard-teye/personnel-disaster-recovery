081a597f077379809a839bb4721288bf1dcc016e
Bernard Teye <bernard.teye@amalitech.com>
2024-07-09 08:11:07 +0000

    make name property in EmergencyTypeDto available to swagger docs

--- src/emergency-type/dto/emergency-type.dto.ts
+++ src/emergency-type/dto/emergency-type.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, Matches } from "class-validator";
 
 export class EmergencyTypeDto {
     @IsNotEmpty()
     @Matches(/^[a-zA-Z ]*$/)
+    @ApiProperty()
     name: string;
 }
