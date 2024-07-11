fcb379f0976bc6996acab9abf6d6d7c46756af2e
Bernard Teye <bernard.teye@amalitech.com>
2024-07-11 15:43:13 +0000

    refactor throwException() in handle-bad-request.util.ts to throw internal errors to reduce code duplications

--- src/shared/util/handle-bad-request.util.ts
+++ src/shared/util/handle-bad-request.util.ts
@@ -1,7 +1,9 @@
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException, InternalServerErrorException } from "@nestjs/common";
 
 export function throwException(error: BadRequestException) {
     if (error instanceof BadRequestException) {
         throw new BadRequestException(error.message);
     }
+
+    throw new InternalServerErrorException("Something went wrong");
 }
