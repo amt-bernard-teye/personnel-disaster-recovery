92235016440a68a0f1c42127d4dd860af09707d9
Bernard Teye <bernard.teye@amalitech.com>
2024-07-25 08:37:34 +0000

    add findAll() route handler in ProjectsController

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ProjectService } from './project.service';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { DataOnlyInterceptor } from 'src/shared/interceptors/data-only.interceptor';
+import { pageParser } from 'src/shared/util/page-parser.util';
 
 @Controller('projects')
-export class ProjectController {}
+@UseGuards(AuthGuard)
+@Roles([Role.PERSONNEL])
+@ApiBearerAuth()
+@ApiTags("Projects")
+export class ProjectController {
+  constructor(
+    private projectService: ProjectService
+  ) {}
+
+  @Get()
+  @UseInterceptors(DataOnlyInterceptor)
+  findAll(@Query("page") page: string, @Query("want") want: string) {
+    let parsedPage = pageParser(page);
+    let wantAll = false;
+
+    if (want === "all") {
+      wantAll = true;
+    }
+
+    return this.projectService.findAll(parsedPage, wantAll);
+  }
+}
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+import { ProjectRepository } from 'src/database/repository/project.repository';
+import { throwException } from 'src/shared/util/handle-bad-request.util';
+
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(
+    private projectRepo: ProjectRepository
+  ) { }
+
+  async findAll(page: number, wantAll: boolean) {
+    try {
+      const projects = await this.projectRepo.findAll(page, wantAll);
+      const count = await this.projectRepo.count();
+
+      return {count, projects};
+    }
+    catch(error) {
+      throwException(error);
+    }
+  }
+}
