2173751a83a4a270db1f898e316ccdafbb56b7c1
Bernard Teye <bernard.teye@amalitech.com>
2024-07-25 07:41:40 +0000

    add create_at column to project model and interface type and changed String type to string

--- src/shared/interface/project.interface.ts
+++ src/shared/interface/project.interface.ts
@@ -1,8 +1,9 @@
 export type Project = {
     id?: number;
-    title: String;
-    description: String;
+    title: string;
+    description: string;
     personnelId: number;
+    created_at?: Date;
 }
 
 export type ProjectProp = {
@@ -10,4 +11,5 @@ export type ProjectProp = {
     title: boolean;
     description: boolean;
     personnelId: boolean;
+    created_at: boolean;
 }
