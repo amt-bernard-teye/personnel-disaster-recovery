d4fe558cea7f1f92973359fe5e75adcff0c3ab2f
Bernard Teye <bernard.teye@amalitech.com>
2024-07-25 13:17:37 +0000

    add findAll() route handler to ManagerController

--- src/manager/manager.controller.ts
+++ src/manager/manager.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
 
-@Controller('manager')
-export class ManagerController {}
+import { ManagerService } from './manager.service';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { pageParser } from 'src/shared/util/page-parser.util';
+import { DataOnlyInterceptor } from 'src/shared/interceptors/data-only.interceptor';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+
+@Controller('managers')
+@UseGuards(RolesGuard)
+@Roles([Role.ADMIN])
+@UseGuards(AuthGuard)
+@ApiTags("Manager")
+@ApiBearerAuth()
+export class ManagerController {
+  constructor(
+    private managerService: ManagerService
+  ) { }
+
+  @Get()
+  @UseInterceptors(DataOnlyInterceptor)
+  findAll(@Query("page") page: string, @Query("want") want: string) {
+    let parsedPage = pageParser(page);
+    let wantAll = false;
+
+    if (want === "all") {
+      wantAll = true;
+    }
+
+    return this.managerService.findAll(parsedPage, wantAll);
+  }
+}
--- src/manager/manager.service.ts
+++ src/manager/manager.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import ManagerRepository from 'src/database/repository/manager.repository';
+import { throwException } from 'src/shared/util/handle-bad-request.util';
 
 @Injectable()
-export class ManagerService {}
+export class ManagerService {
+  constructor(
+    private managerRepo: ManagerRepository
+  ) { }
+
+  async findAll(page: number, wantAll: boolean) {
+    try {
+      const managers = await this.managerRepo.findAll(page, wantAll);
+      const count = await this.managerRepo.count();
+
+      return {count, managers};
+    }
+    catch(error) {
+      throwException(error);
+    }
+  }
+}
